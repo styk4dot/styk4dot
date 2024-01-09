4d5468eab1f7670eac3850003166d1a1a1255e10
stykadot <110493475+stykadot@users.noreply.github.com>
2024-01-09 17:27:19 -0500

    vuhhhhcambooobia

---
+++ my-app/app/page.tsx
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Brian Griffin Scrotum</h1>
+      <iframe 
+        style={{border: 0, width: '100%', height: '42px'}}
+        src="https://bandcamp.com/EmbeddedPlayer/album=3509547392/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+        seamless>
+        <a href="https://codafiles.bandcamp.com/album/i-can-do-whatever-i-want-a-piacere-if-ya-nasty">
+          I can do whatever I want (A piacere if ya nasty) by codafiles
+        </a>
+      </iframe>
+    </div>
+  );
+}
+
+export default Page;
